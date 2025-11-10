04a1a5fa5ec0e06feca1e8c281cfd3d1c2feb311
n3m0-ke <michaelodhiambo38@gmail.com>
2025-11-10 14:04:28 +0300

    Trying to add a ping somewhere

--- app/analysis/page.tsx
+++ app/analysis/page.tsx
@@ -182,9 +182,18 @@ export default function AnalysisPage() {
         </div>
         <div className="text-gray-800 font-medium">
           Balance:{' '}
-          {month
-            ? `${remainingBudget.toLocaleString()} / ${totalBudget.toLocaleString()} KES`
-            : '--- / ---'}
+          {month ? (
+            <span className="flex items-center gap-2">
+              {`${remainingBudget.toLocaleString()} / ${totalBudget.toLocaleString()} KES`}
+              <span className="relative flex size-3">
+                <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-sky-400 opacity-75"></span>
+                <span className="relative inline-flex size-3 rounded-full bg-sky-500"></span>
+              </span>
+            </span>
+          ) : (
+            '--- / ---'
+          )}
+
         </div>
       </div>
 
